07f7a945b638c0ab6429f56f6d50b50f5f05462d
Paskal W <paskal.wolski@gmail.com>
2026-03-29 13:17:49 +0200

    Starts new session if we try to start a new lap which conflicts with teh current tracked lap

--- LapController.py
+++ LapController.py
@@ -185,6 +185,9 @@ class LapController:
 
     def start_lap(self, lap_number, lap_time=None):
         log("[controller] start_lap: {} (lap_time={})".format(lap_number, lap_time))
+        if lap_number <= self.current_lap:
+            log('[controller] Lap Conflict: re-starting current lap {}. Forcing Session Start'.format(lap_number))
+            self.start_session()
         self.end_lap(lap_time)
         self.current_lap = lap_number
         # TODO: Add logic for catching a start behind the s/f line
