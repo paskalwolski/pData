bfa8d4ead44ee8abf543d369534d930c56cb7be6
Paskal W <paskal.wolski@gmail.com>
2026-04-29 14:12:56 +0200

    Patches for the Lap Status Display
    - Fixes priority render for pit/invalid text

--- src/data_displays/LapStatus.py
+++ src/data_displays/LapStatus.py
@@ -1,4 +1,5 @@
 import ac  # type: ignore
+from src.plogging import pLogger
 from src.services.AppConfig import app_config
 
 RED = (
@@ -9,7 +10,7 @@ RED = (
 
 GREEN = (
     0.2,
-    0.1,
+    1.0,
     0.2,
 )
 
@@ -28,7 +29,6 @@ class LAP_STATE:
 
 LAP_STATE_COLORS = {LAP_STATE.ok: GREEN, LAP_STATE.pit: BLUE, LAP_STATE.invalid: RED}
 
-
 class LapStatus:
 
     def __init__(self):
@@ -37,7 +37,7 @@ class LapStatus:
         self.is_invalid = False
 
         # Lap History Tracking
-        self.lap_history_data = []  # type: list[str | None]
+        self.lap_history_data = [None for _ in range(5)]  # type: list[str | None]
         self._setup_ui()
 
     def register_lap(self, lap_number):
@@ -50,6 +50,8 @@ class LapStatus:
         self.current_lap = lap_number
         self.is_invalid = False
         self.is_pit = False
+
+        self._update_display()
 
     def register_session(self):
         self.current_lap = None
@@ -77,9 +79,6 @@ class LapStatus:
         ac.drawBackground(self.app, 0)
         ac.setTitle(self.app, " ")  # Empty title so it's not distracting
 
-        # Position the window on screen
-        ac.setPosition(self.app, 200, 100)
-
         # Hide the title bar and background
         ac.drawBorder(self.app, 0)
         ac.setIconPosition(self.app, -10000, -10000)
@@ -93,14 +92,15 @@ class LapStatus:
         ac.setVisible(self.label, 1)
 
         # Create the Lap History row
-        self.lap_history_row = [ac.addLabel(self.app, " ") for _ in range(5)]
+        self.lap_history_row = [ac.addLabel(self.app, "") for _ in range(5)]
         start_pos = 0
         width = 60
         height = 10
         for lap_block in self.lap_history_row:
             ac.setPosition(lap_block, start_pos, 80)
             ac.setSize(lap_block, width, height)
-            ac.setBackgroundColor(lap_block, *GREEN)
+            ac.setBackgroundOpacity(lap_block, 0)
+            start_pos += width
 
     def _clear_lap_history(self):
         self.lap_history_data = [None for _ in range(5)]
@@ -123,12 +123,12 @@ class LapStatus:
         Trigger an update to the display when the state changes,
         including when a new lap is registered
         """
-        if self.is_pit:
-            notification_text = "LAP {} PIT".format(self.current_lap)
-            self._set_text(notification_text, BLUE)
-        elif self.is_invalid:
+        if self.is_invalid:
             notification_text = "LAP {} INVALID".format(self.current_lap)
             self._set_text(notification_text, RED)
+        elif self.is_pit:
+            notification_text = "LAP {} PIT".format(self.current_lap)
+            self._set_text(notification_text, BLUE)
         else:
             self._set_text("")
 
@@ -136,8 +136,8 @@ class LapStatus:
         for i, lap_block in enumerate(self.lap_history_row):
             lap_state = self.lap_history_data[i]
             if lap_state:
-                ac.setBackgroundOpacity(lap_block, 1)
                 ac.setBackgroundColor(lap_block, *LAP_STATE_COLORS[lap_state])
+                ac.setBackgroundOpacity(lap_block, 1)
             else:
                 ac.setBackgroundOpacity(lap_block, 0)
 
@@ -145,7 +145,8 @@ class LapStatus:
         # type: (str, tuple[float, float, float] | None, float) -> None
         ac.setText(self.label, text)
         if color:
-            ac.setFontColor(self.label, *color, opacity)
+            effective_color = color + (opacity,)
+            ac.setFontColor(self.label, *effective_color)
 
 
 lap_status_display = LapStatus()
