a08c483b9e3d5f88ff684b44cefa23c09cabeaa1
Paskal W <paskal.wolski@gmail.com>
2026-05-19 16:58:44 +0200

    Added more logging to worker class

--- src/worker.py
+++ src/worker.py
@@ -16,24 +16,31 @@ class Worker:
         log("Worker started")
 
     def enqueue(self, task):
+        log("Queue Size: {}".format(self._queue.qsize()))
+        log("Unfinished Tasks: {}".format(self._queue.unfinished_tasks))
         self._queue.put(task)
 
     def stop(self):
+        log("Breaking Task Queue...")
         self._queue.put(None)
+        log("Logging Thread State")
         for t in threading.enumerate():
             log(t.name, t.is_alive())
+        log("Joining Thread...")
         self._thread.join()
-        log("Stopped")
+        log("Thread Joined")
 
     def _run(self):
         while True:
             task = self._queue.get()
             if task is None:
+                log("Task Queue Interrupted")
                 break
             try:
                 task()
             except:  # pylint: disable=W0702
                 log(traceback.format_exc())
+        log("Thread Stopped")
 
 
 worker = Worker()
