29bef28bc995c5fbd5d41f01928a86a29538701a
Paskal W <paskal.wolski@gmail.com>
2026-06-18 20:15:32 +0200

    Adds timestamp to logging function

--- src/plogging.py
+++ src/plogging.py
@@ -1,9 +1,14 @@
 import ac
+import datetime
+
+
+def _ts():
+    return datetime.datetime.now().strftime("%H:%M:%S.%f")[:10]
 
 
 # TODO: Turn this into a class that gets init'ed with the module name
 # [pData | {module_name}] {function_name}: {log}
-def log(text: str, *args):
+def log(text, *args):
     ac.log("[pData] {}".format(text))
 
 
@@ -12,9 +17,9 @@ class pLogger:
         self.module = module_name
 
     def log(self, *args):
-        ac.log("[pData | {}] | {}".format(self.module, " | ".join([str(a) for a in list(args)])))
+        ac.log("[pData | {} | {}] | {}".format(_ts(), self.module, " | ".join([str(a) for a in list(args)])))
 
     def worker_log(self, *args):
         ac.log(
-            "[pData | worker | {}] | {}".format(self.module, " | ".join([str(a) for a in list(args)]))
+            "[pData | {} | worker | {}] | {}".format(_ts(), self.module, " | ".join([str(a) for a in list(args)]))
         )
